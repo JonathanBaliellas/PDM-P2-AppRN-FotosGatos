e4bd0b090bc6eb80b42d3b7b055c2f70ffa46b50
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-06-08 00:20:33 -0300

    feat(p2-pdm): cria o botão que busca foto de gatos
    - Implementa o método para exibir a imagem
    - Instala o pacote axios para fazer as chamadas à API

--- gatix/App.tsx
+++ gatix/App.tsx
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import axios from 'axios';
+import { useState } from 'react';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+
+interface Imagem {
+  url: string
+}
 
 export default function App() {
+  const [imagem, setImagem] = useState<Imagem>()
+
+  const exibirImagem = async () => {
+    const url = 'https://api.thecatapi.com/v1/images/search'
+    const resposta = await axios.get(url)
+    setImagem(resposta.data[0])
+  }
+
   return (
     <View style={styles.container}>
       <Text>Bem-vindo ao Gatix!</Text>
-      <StatusBar style="auto" />
+      <Pressable
+        style={styles.button}
+        onPress={exibirImagem}
+      >
+        <Text
+          style={styles.buttonText}
+        >Buscar gatinhos!</Text>
+      </Pressable>
+      <View>
+        <Image 
+          style={styles.image}
+          source={{uri: imagem?.url}}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  button:{
+    width: '80%',
+    backgroundColor: '#0096F3',
+    padding: 32,
+    borderRadius: 8,
+    margin: 16
+  },
+  buttonText:{
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 24
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    height: 150,
+    width: 150,
+    borderRadius: 16
+  }
 });
