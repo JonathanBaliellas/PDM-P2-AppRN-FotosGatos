dfbe8e532dc3bb00513c44fb8f6af2c2e0e7be7e
Jonathan Baliellas <101955148+JonathanBaliellas@users.noreply.github.com>
2024-06-14 03:13:21 -0300

    feat(p2-pdm): configura o backend e implementa endpoint
    - Cria o backend
    - Recebe requisições e envia ao TheCatAPI

---
+++ gatix-backend/src/app.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import express from 'express'
+import axios from 'axios'
+const app = express()
+app.use(express.json())
+const API_KEY = process.env.API_KEY
+app.get('/search', async (req, res) => {
+    const client = axios.create({
+        baseURL: 'https://api.thecatapi.com/v1/images/',
+        headers: {
+            'x-api-key': API_KEY
+        }
+    })
+
+    const resposta = await client.get('/search', {
+        params: {
+            limit: 5
+        }
+    })
+
+    res.json(resposta.data)
+})
+const porta: number = 3000 //Alterar para uma porta livre, se o servidor não subir
+app.listen(porta, () => {
+    console.log(`Executando na porta ${porta}`)
+})
